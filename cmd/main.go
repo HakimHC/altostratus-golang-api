8da23fa503ccf0d0c442cc38c46dc9dd10cad83b
HakimHC <ahmedh90111@gmail.com>
2024-06-03 23:32:23 +0200

    Main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
-package cmd
+package main
 
 import (
+	"github.com/HakimHC/altostratus-golang-api/middleware"
 	"github.com/HakimHC/altostratus-golang-api/models"
+	"github.com/HakimHC/altostratus-golang-api/routes"
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
@@ -9,6 +11,7 @@ import (
 func main() {
 	e := echo.New()
 	e.Validator = &models.CustomValidator{Validator: validator.New()}
-	routes.AuthRoute(e)
+	e.Use(middleware.JWTMiddleware)
+	routes.AsteroidRoutes(e)
 	e.Logger.Fatal(e.Start(":80"))
 }
