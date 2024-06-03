cd174c7ef565b7d9feb167763c60ac442f8f42ef
HakimHC <ahmedh90111@gmail.com>
2024-06-03 23:32:32 +0200

    Updated models

--- models/asteroids.go
+++ models/asteroids.go
@@ -1,9 +1,15 @@
 package models
 
-//{
-//  "name": "Apophis",
-//  "diameter": 370,
-//  "discovery_date": "13-04-2004",
-//  "observations": "Leve cambio de trayectoria",
-//  "distances": []
-//}
+type DistanceStruct struct {
+	Date     string  `json:"date" validate:"required"`
+	Distance float64 `json:"distance" validate:"required,gte=0"`
+}
+
+type Asteroid struct {
+	ID            string           `json:"id" validate:"required"`
+	Name          string           `json:"name" validate:"required"`
+	Diameter      float64          `json:"diameter" validate:"required,gte=0"`
+	DiscoveryDate string           `json:"discovery_date" validate:"required"`
+	Observations  string           `json:"observations,omitempty" validate:"omitempty,min=1"`
+	Distances     []DistanceStruct `json:"distances,omitempty" validate:"omitempty,dive"`
+}
