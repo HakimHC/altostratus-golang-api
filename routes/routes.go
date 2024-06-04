669e6b6aeafc952490904281e0da820a21fbaf51
HakimHC <ahmedh90111@gmail.com>
2024-06-04 20:48:41 +0200

    Reactivating auth middleware

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/HakimHC/altostratus-golang-api/controllers"
+	"github.com/HakimHC/altostratus-golang-api/middleware"
 	"github.com/labstack/echo/v4"
 )
 
 func AsteroidRoutes(e *echo.Echo) {
 	api := e.Group("/api/v1", serverHeader)
 
-	//api.Use(middleware.JWTMiddleware)
+	api.Use(middleware.JWTMiddleware)
 
 	api.GET("/asteroids", controllers.GetAllAsteroids)
 	api.GET("/asteroids/:id", controllers.GetAsteroidById)
